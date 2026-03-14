d50e0d1ed9c60dd7313fd7aaef6c122a002964b0
4sciencee <palomarescarlitos24@gmail.com>
2026-03-13 19:55:23 -0700

    add docker

--- obj/Debug/net8.0/Bus410Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bus410Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bus410Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed303c5ef73f47fde239a7b6a89f9ba301945db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92aa55ad37ea6f865dec0d8b6deb0a96dbf673b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bus410Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bus410Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
